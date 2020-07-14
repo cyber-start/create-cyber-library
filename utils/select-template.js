abf3743dcb544500b9de32155af40c94ecdd72bd
shicheng.yu <shicheng.yu@fawaimanongdeMacBook-Pro.local>
2020-07-14 17:39:23 +0800

    2020年07月14日 17:23的快速提交

--- utils/select-template.js
+++ utils/select-template.js
@@ -2,22 +2,20 @@ const inquirer = require("inquirer");
 
 
 module.exports = async () => {
-  try {
-    const { template } = await inquirer.prompt({
-      type: "list",
-      name: "template",
-      defaultValue: "dva-app",
-      message: "选择拉取的脚手架类型:",
-      choices: [{
-        name: "dva-app",
-        value: "https://github.com/nice-web-work/dva-spa-project"
-      }, {
-        name: "es6-babel-library",
-        value: "https://github.com/nice-web-work/es6-babel-library"
-      }]
-    });
-    return template;
-  } catch (error) {
-    throw error;
-  }
+
+  const { template } = await inquirer.prompt({
+    type: "list",
+    name: "template",
+    defaultValue: "dva-app",
+    message: "选择拉取的脚手架类型:",
+    choices: [{
+      name: "dva-app",
+      value: "https://github.com/nice-web-work/dva-spa-project"
+    }, {
+      name: "es6-babel-library",
+      value: "https://github.com/nice-web-work/es6-babel-library"
+    }]
+  });
+  return template;
+
 };
