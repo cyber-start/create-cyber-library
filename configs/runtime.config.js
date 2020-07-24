7fccdbe08222c2a76cb8e152cf523485d045d861
shicheng.yu <shicheng.yu@fawaimanongdeMacBook-Pro.local>
2020-07-24 21:49:30 +0800

    2.1.7版本发布

--- configs/runtime.config.js
+++ configs/runtime.config.js
@@ -1,16 +1,11 @@
 
 module.exports = [{
-  name: "es6-babel-library",
+  name: "cyber-babel-library",
   value: {
     devDependencies: {
       "@cyber-tools/lib-cli": "*",
       "@cyber-tools/spa-cli": "*"
     },
     remote: "https://github.com/cyber-scaffold/es6-babel-library"
-  }
-}, {
-  name: "cli-project",
-  value: {
-    remote: "https://github.com/cyber-scaffold/cli-project"
   }
 }];
