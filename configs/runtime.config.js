e3ba839bcd955a0e0597cee422f79dffdd655635
shicheng.yu <yushicheng@hztianque.com>
2020-08-26 17:07:53 +0800

    2.1.12版本发布

--- configs/runtime.config.js
+++ configs/runtime.config.js
@@ -1,6 +1,6 @@
 
 module.exports = [{
-  name: "cyber-babel-library",
+  name: "babel-library",
   value: {
     devDependencies: {
       "@cyber-tools/lib-cli": "*",
@@ -8,4 +8,19 @@ module.exports = [{
     },
     remote: "https://github.com/cyber-scaffold/es6-babel-library"
   }
+}, {
+  name: "typescript-library",
+  value: {
+    devDependencies: {
+      "@cyber-tools/spa-cli": "*"
+    },
+    remote: "https://github.com/cyber-scaffold/ts-library"
+  }
+}, {
+  name: "component-library",
+  devDependencies: {
+    "@cyber-tools/spa-cli": "*",
+    "@cyber-tools/component-build-tools": "*"
+  },
+  remote: "https://github.com/cyber-scaffold/typescript-component-library"
 }];
